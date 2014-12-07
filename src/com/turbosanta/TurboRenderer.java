9781be7be89f0c7aa3721fc741b0abfd030acd00
Diego Waxemberg <diego.waxemberg@gmail.com>
2014-12-06 21:31:32 -0500

    removed unused code

--- src/com/turbosanta/TurboRenderer.java
+++ src/com/turbosanta/TurboRenderer.java
@@ -1,6 +1,5 @@
 package com.turbosanta;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.util.concurrent.SynchronousQueue;
@@ -46,7 +45,6 @@ public class TurboRenderer {
 		@Override
 		public void run() {
 			System.out.println("RenderThread Spinning up");
-			int num = 100;
 			while (true) {
 				Integer[] bitmap = null;
 				try {
@@ -63,28 +61,6 @@ public class TurboRenderer {
 
 					Graphics g = frame.getGraphics();
 					g.drawImage(image, 0, 0, frame.getWidth(), frame.getHeight(), 0, 0, width, height, null);
-					if (num-- == 0 && false) {
-						g.setColor(Color.WHITE);
-						g.fillRect(0, 0, frame.getWidth(), frame.getHeight());
-						g.setColor(Color.BLUE);
-						for (int y = 0; y < height; y++) {
-							for (int x = 0; x < width; x++) {
-								int pixelVal = 0x000000FF & bitmap[x+y*width];
-								String pixelChar = "";
-								if (pixelVal <= 62 ) {
-									pixelChar = "#";
-								} else if (pixelVal <= 127) {
-									pixelChar = "*";
-								} else if (pixelVal <= 191) {
-									pixelChar = ".";
-								} else {
-									pixelChar = " ";
-								}
-								g.drawChars(pixelChar.toCharArray(), 0, 1, frame.getWidth() / width * x, frame.getHeight() / height * y);
-							}
-						}
-						num = 100;
-					}
 				}
 			}
 		}
